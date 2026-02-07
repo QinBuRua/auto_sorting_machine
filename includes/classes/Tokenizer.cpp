927dbc66a68dc379c6beba7b76979d62fcfcd3e6
QinBu_Rua <qmr_1104@163.com>
2026-02-07 18:51:52 +0800

    (todo)新增(Tokenizer): 新增load_from_file方法用于读取MarkovModel文件

--- includes/classes/Tokenizer.cpp
+++ includes/classes/Tokenizer.cpp
@@ -3,5 +3,20 @@
 //
 
 #include "Tokenizer.h"
+#include "details/Tokenizer/ReadModelHelper.h"
 
 using namespace QinBuRua::auto_sorting_machine;
+
+Tokenizer::Tokenizer(const MarkovChainModel& model) {
+   m_MarkovModel = model;
+}
+
+Tokenizer::Tokenizer(MarkovChainModel&& model) noexcept {
+   m_MarkovModel = std::move(model);
+}
+
+void Tokenizer::load_from_file(const std::string& filename) {
+   using details::tokenizer::ReadModelHelper;
+   ReadModelHelper helper;
+
+}
--- includes/classes/Tokenizer.h
+++ includes/classes/Tokenizer.h
@@ -5,15 +5,19 @@
 #ifndef AUTO_SORTING_MACHINE_TOKENIZER_H
 #define AUTO_SORTING_MACHINE_TOKENIZER_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "MarkovChainModel.h"
 
-namespace QinBuRua::auto_sorting_machine{
+namespace QinBuRua::auto_sorting_machine {
 
 class Tokenizer {
 public:
+   explicit Tokenizer(const MarkovChainModel& model);
+   explicit Tokenizer(MarkovChainModel&& model) noexcept;
+
+   void load_from_file(const std::string& filename); //todo
 
 private:
    MarkovChainModel m_MarkovModel;
