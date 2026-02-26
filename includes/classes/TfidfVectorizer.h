5639d22f53870562282f431b12bcc16062ab999e
QinBu_Rua <qmr_1104@163.com>
2026-02-26 23:00:37 +0800

    新增(TfidfVectorizer): 新增大量using预声明可能使用的类型

--- includes/classes/TfidfVectorizer.h
+++ includes/classes/TfidfVectorizer.h
@@ -6,16 +6,32 @@
 #define AUTO_SORTING_MACHINE_INCLUDES_CLASSES_TFIDFVECTORIZER_H
 
 #include <memory>
+#include <stdfloat>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <valarray>
-
-#include "json.hpp"
+#include <vector>
 
 namespace QinBuRua::auto_sorting_machine {
 
 class TfidfVectorizer {
+   //todo:未完成的类
 public:
-   using Word     = std::u16string;
-   using Sentence = std::u16string;
+   using Word                = std::u16string;
+   using Document            = std::vector<Word>;
+   using Documents           = std::vector<Document>;
+   using Category            = std::u16string;
+   using ClassifiedDocuments = std::unordered_map<Category, Documents>;
+
+   using Vocabulary       = std::unordered_set<Word>;
+   using RawVector        = std::valarray<uint32_t>;
+   using WordFrequency    = std::unordered_map<Word, uint32_t>;
+   using TfVector         = std::valarray<std::float32_t>;
+   using IdfVector        = std::valarray<std::float32_t>;
+   using TfidfVector      = std::valarray<std::float32_t>;
+   using TfidfVectors     = std::vector<TfidfVector>;
+   using DocumentsVectors = std::unordered_set<Category, TfidfVectors>;
 
 public:
 
