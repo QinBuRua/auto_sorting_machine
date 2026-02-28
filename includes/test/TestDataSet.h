da851a835f8dcf4f507a7c0810cebe0022959acb
QinBu_Rua <qmr_1104@163.com>
2026-02-28 12:34:31 +0800

    新增(TestDataSet.h/cpp): 1.为CMake添加编译支持 2.新增方法test::create_classified_documents_test_set()用于测试

---
+++ includes/test/TestDataSet.h
@@ -0,0 +1,16 @@
+//
+// Created by QinBu_Rua on 2026/2/28.
+//
+
+#ifndef AUTO_SORTING_MACHINE_INCLUDES_TEST_TESTDATASET_H
+#define AUTO_SORTING_MACHINE_INCLUDES_TEST_TESTDATASET_H
+
+#include "classes/TfidfVectorizer.h"
+
+namespace QinBuRua::auto_sorting_machine::test {
+
+TfidfVectorizer::ClassifiedDocuments create_classified_documents_test_set();
+
+}
+
+#endif //AUTO_SORTING_MACHINE_INCLUDES_TEST_TESTDATASET_H
